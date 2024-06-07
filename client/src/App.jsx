730c3a7e2bbf8edd9f7b78add8607af3f20d1415
devpaths <devangpathak752001@gmail.com>
2024-06-07 13:38:13 +0530

    repo change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,10 @@ import Home from "./pages/Home"
 import About from './pages/About';
 import MyAccount from './pages/MyAccount';
 import Products from './pages/Products';
+import Header from './Components/Header';
 function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header className='bg-slate-200' shadow-md>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>MagicSprings</span>
+        </h1>
+          </Link>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type='text' placeholder='Search our products' className='bg-transparent focus:outline-none sm:w-64 w-24'/>
+            <FaSearch className="text-slate-600"/>
+        </form>
+        <ul className="flex gap-6 text-slate-600 font-bold">
+        <Link to='/Products'> <li className="hidden sm:inline hover:px-1  transition-all duration-500 hover:text-slate-800 ">Bathroom</li> </Link>
+        <Link to='/Products'>  <li className="hidden sm:inline hover:px-1  rounded-lg transition-all duration-300 hover:text-slate-800">Kitchen</li> </Link>
+        <Link to='/About'>   <li className="hidden sm:inline hover:px-1  rounded-lg transition-all duration-300 hover:text-slate-800">About Us</li> </Link>
+        <Link to='/About'>   <li className=" sm:inline sm:text-base text-xs hover:px-1  rounded-lg transition-all duration-300 hover:text-slate-800">My Profile</li> </Link>
+        </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
