d65fbfb7d43ea76874f1f184b5a4314256adf5f4
devpaths <devangpathak752001@gmail.com>
2024-06-07 15:11:42 +0530

    MongoDB connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL).then(() => {
+    console.log("MONGODB connected bhai 🥳🎉🎊")   
+})
+.catch((err)=>{
+    console.log('Database nhi hua connect 😢')
+})
+
 
 const app = express();
 
 app.listen(3000 , () => {
-    console.log("Server 3000 me chla hai")
+    console.log("Server 3000 me chlla hai")
 })
