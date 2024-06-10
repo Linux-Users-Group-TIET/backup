6f114178c101d3f92375fa6b93933dba08d79ba2
devpaths <devangpathak752001@gmail.com>
2024-06-10 14:21:36 +0530

    completed signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home"
 import About from './pages/About';
-import signIn from './pages/SignIn';
-
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import Products from './pages/Products';
 import Header from './Components/Header';
 function App() {
@@ -13,8 +13,8 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/products' element={<Products/>}/>
-      <Route path='/signup' element={<signUp/>}/>
-      <Route path='/signin' element={<signIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
   </Routes>
   </BrowserRouter>
 }
