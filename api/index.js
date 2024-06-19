94629fccfa91c0ffb6a3189e6798ea6f3299fb14
devpaths <devangpathak752001@gmail.com>
2024-06-20 02:54:10 +0530

    added pre deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.mongourl,{
@@ -17,6 +18,7 @@ mongoose.connect(process.env.mongourl,{
     console.log('Database nhi hua connect 😢')
 })
 
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -28,15 +30,16 @@ app.listen(4000 , () => {
 })
 const cors = require('cors');
 // Allow all origins
-app.use(cors());
-// Allow specific origin(s)
-app.use(cors({
-  origin: 'https://magicspringsfrontend-git-main-devs-projects-6847079c.vercel.app/',
-  credentials: true,
-}));
+
 app.use('/api/user', userRouter);
 app.use('/api/auth' , authRouter);
 app.use('/api/listing' , listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
     const message  =err.message || "Internal Server mei error";
